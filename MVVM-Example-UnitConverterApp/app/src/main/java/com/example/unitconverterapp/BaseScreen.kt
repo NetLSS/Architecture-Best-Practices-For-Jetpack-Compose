eaab68c165f30bd0c5a4026740e84d6ca99cc5de
sangsulee <sangsulee@gmarket.com>
2024-03-13 16:15:24 +0900

    Feat: BaseScreen: add screen

--- MVVM-Example-UnitConverterApp/app/src/main/java/com/example/unitconverterapp/BaseScreen.kt
+++ MVVM-Example-UnitConverterApp/app/src/main/java/com/example/unitconverterapp/BaseScreen.kt
@@ -1,7 +1,12 @@
 package com.example.unitconverterapp
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
@@ -11,4 +16,10 @@ fun BaseScreen(
 ) {
     // 변하지 않으므로 State 로 둘 필요가 없다.
     val list = converterViewModel.getConversions()
+
+    Column(modifier = modifier.padding(30.dp)) {
+        TopScreen(list)
+        Spacer(modifier = modifier.height(20.dp))
+        HistoryScreen()
+    }
 }
