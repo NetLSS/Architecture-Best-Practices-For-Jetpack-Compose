a88a7bb370801b42900d058e7b6ac5fe9653d3ae
sangsulee <sangsulee@gmarket.com>
2024-03-18 15:36:00 +0900

    Refactor: package 별로 나눔

--- MVVM-Example-UnitConverterApp/app/src/main/java/com/example/unitconverterapp/compose/ConversionMenu.kt
+++ MVVM-Example-UnitConverterApp/app/src/main/java/com/example/unitconverterapp/compose/ConversionMenu.kt
@@ -1,4 +1,4 @@
-package com.example.unitconverterapp
+package com.example.unitconverterapp.compose
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -25,6 +25,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.toSize
+import com.example.unitconverterapp.data.Conversion
 
 @Composable
 fun ConversionMenu(
