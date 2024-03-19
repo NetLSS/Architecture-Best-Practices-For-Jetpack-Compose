812f9e92396a791e3c1ea7e50e9b3992940fa156
sangsulee <sangsulee@gmarket.com>
2024-03-19 13:33:32 +0900

    Refactor: ConverterDatabase: build database

--- MVVM-Example-UnitConverterApp/app/src/main/java/com/example/unitconverterapp/data/ConverterDatabase.kt
+++ MVVM-Example-UnitConverterApp/app/src/main/java/com/example/unitconverterapp/data/ConverterDatabase.kt
@@ -15,16 +15,12 @@ abstract class ConverterDatabase : RoomDatabase() {
         private var INSTANCE: ConverterDatabase? = null
 
         fun getInstance(context: Context): ConverterDatabase {
-            synchronized(this) {
-                var instance = INSTANCE
-                if (instance == null) {
-                    instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        ConverterDatabase::class.java,
-                        "converter_data_database"
-                    ).build()
-                }
-                return instance
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    ConverterDatabase::class.java,
+                    "converter_data_database"
+                ).build().also { INSTANCE = it }
             }
         }
     }
