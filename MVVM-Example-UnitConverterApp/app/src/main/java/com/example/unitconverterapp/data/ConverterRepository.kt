9a12dfe5bbe435f563c1cf7534350c0fb0ebd46c
sangsulee <sangsulee@gmarket.com>
2024-03-19 13:31:13 +0900

    Feat: add ConverterRepository

---
+++ MVVM-Example-UnitConverterApp/app/src/main/java/com/example/unitconverterapp/data/ConverterRepository.kt
@@ -0,0 +1,10 @@
+package com.example.unitconverterapp.data
+
+import kotlinx.coroutines.flow.Flow
+
+interface ConverterRepository {
+    suspend fun insertResult(result: ConversionResult)
+    suspend fun deleteResult(result: ConversionResult)
+    suspend fun deleteAllResults()
+    fun getSavedResult(): Flow<List<ConversionResult>>
+}
