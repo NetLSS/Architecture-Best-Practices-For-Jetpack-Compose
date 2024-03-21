7c133466da4d7f87c1493721cba668c7627da303
sangsulee <sangsulee@gmarket.com>
2024-03-21 10:15:28 +0900

    Feat: Dependency Injection With Hilt

--- MVVM-Example-UnitConverterApp/app/src/main/java/com/example/unitconverterapp/ConverterViewModelFactory.kt
+++ MVVM-Example-UnitConverterApp/app/src/main/java/com/example/unitconverterapp/ConverterViewModelFactory.kt
@@ -3,8 +3,9 @@ package com.example.unitconverterapp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider.NewInstanceFactory
 import com.example.unitconverterapp.data.ConverterRepository
+import javax.inject.Inject
 
-class ConverterViewModelFactory(private val repository: ConverterRepository) :
+class ConverterViewModelFactory @Inject constructor(private val repository: ConverterRepository) :
     NewInstanceFactory() {
     override fun <T : ViewModel> create(modelClass: Class<T>): T =
         ConverterViewModel(repository) as T
--- MVVM-Example-UnitConverterApp/app/src/main/java/com/example/unitconverterapp/MainActivity.kt
+++ MVVM-Example-UnitConverterApp/app/src/main/java/com/example/unitconverterapp/MainActivity.kt
@@ -11,13 +11,20 @@ import com.example.unitconverterapp.compose.BaseScreen
 import com.example.unitconverterapp.data.ConverterDatabase
 import com.example.unitconverterapp.data.ConverterRepositoryImpl
 import com.example.unitconverterapp.ui.theme.UnitConverterAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var factory: ConverterViewModelFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val dao = ConverterDatabase.getInstance(application).converterDAO
-        val repository = ConverterRepositoryImpl(dao)
-        val factory = ConverterViewModelFactory(repository)
+        
+
+        
         setContent {
             UnitConverterAppTheme {
                 // A surface container using the 'background' color from the theme
